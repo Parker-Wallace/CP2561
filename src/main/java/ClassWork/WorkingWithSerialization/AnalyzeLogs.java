181c083e47a822cf79d1f68a63b90f336fdceb50
Parker.Wallace <wallace.parker@hotmail.com>
2024-05-28 10:24:37 -0230

    fixed file pathing

--- src/main/java/ClassWork/WorkingWithSerialization/AnalyzeLogs.java
+++ src/main/java/ClassWork/WorkingWithSerialization/AnalyzeLogs.java
@@ -1,47 +1,38 @@
 package ClassWork.WorkingWithSerialization;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.security.spec.ECField;
 import java.util.Scanner;
+import javax.xml.bind.JAXB;
 
 public class AnalyzeLogs {
     public static void main(String[] args) {
-        Path path = Paths.get("./logs");
-        if (Files.exists(path))
-        {
-            System.out.println("Folder exists");
-
+        Path path = Paths.get("src/main/java/ClassWork/WorkingWithSerialization/logs");
+        if (Files.exists(path)) {
             try {
                 DirectoryStream<Path> directoryStream =
                         Files.newDirectoryStream(path);
                 int totalFiles = 0;
                 int totalGuesses = 0;
                 for (Path p : directoryStream) {
-                    totalFiles++;
-                    System.out.println(p);
-                    Scanner input = new Scanner(p);
-                    while( input.hasNext() ) {
-                        totalGuesses++;
-                        String line = input.nextLine();
-                        System.out.println(line);
-                    }
+                    BufferedReader input = Files.newBufferedReader(p);
+                    GameLog gameLogs = JAXB.unmarshal(input, GameLog.class);
+                    totalGuesses += gameLogs.getGuesses().size();
+                    totalFiles ++;
                 }
-                System.out.println("Total guesses " + totalGuesses);
-                System.out.println("Total files " + totalFiles);
-                System.out.println("Average guesses: " + (float)totalGuesses/totalFiles);
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-                System.err.println(e.getMessage());
+                System.out.println("total games played: " + totalFiles);
+                System.out.println("total guesses across games: " + totalGuesses);
+                System.out.println("average guesses: " + totalGuesses/totalFiles);
+            } catch (Exception e) {
+                System.out.println(e);
             }
         }
         else {
-            System.out.println("Can't find folder to process logs");
+            System.out.println("files not found");
         }
-
-
-    }
-}
+    }}
