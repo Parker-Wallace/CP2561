9cec2784daceb44207edb1a70f057fc0fe054e64
Parker.Wallace <wallace.parker@hotmail.com>
2024-05-31 12:31:47 -0230

    added parsing of lines to seperate words and point values

--- src/main/java/Assignments/A2Serialization/PhishingScanner/PhishingScanner.java
+++ src/main/java/Assignments/A2Serialization/PhishingScanner/PhishingScanner.java
@@ -4,17 +4,28 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class PhishingScanner {
     public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        List<Integer> points = new ArrayList<>();
         BufferedReader reader;
-        String path = "Assignments/A2Serialization/PhishingScanner/PhisingTerms.txt";
+
+        Path path = Paths.get("src/main/java/Assignments/A2Serialization/PhishingScanner/PhishingTerms.txt");
         try {
-            reader = new BufferedReader(new FileReader(path));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            List<String> allLines = Files.readAllLines(path);
+            for (String line : allLines) {
+                String[] parts = line.split("\\s+");
+                words.add(parts[0]);
+                points.add(Integer.valueOf(parts[1]));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
