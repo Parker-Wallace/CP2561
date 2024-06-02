096b68231c1c5901c66fee2fa3ab35f8b1112c15
Parker Wallace <wallace.parker@hotmail.com>
2024-06-02 21:16:58 -0230

    added try with resources

--- src/main/java/ClassWork/WorkingWithSerialization/AnalyzeLogs.java
+++ src/main/java/ClassWork/WorkingWithSerialization/AnalyzeLogs.java
@@ -1,22 +1,15 @@
 package ClassWork.WorkingWithSerialization;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.Path;
-import java.security.spec.ECField;
-import java.util.Scanner;
+import java.nio.file.*;
 import javax.xml.bind.JAXB;
 
 public class AnalyzeLogs {
     public static void main(String[] args) {
         Path path = Paths.get("src/main/java/ClassWork/WorkingWithSerialization/logs");
         if (Files.exists(path)) {
-            try {
-                DirectoryStream<Path> directoryStream =
-                        Files.newDirectoryStream(path);
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path))
+            {
                 int totalFiles = 0;
                 int totalGuesses = 0;
                 for (Path p : directoryStream) {
