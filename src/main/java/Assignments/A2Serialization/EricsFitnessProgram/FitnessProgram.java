9747a1625071dda2871cea13b4e1de66b78abbcf
Parker Wallace <wallace.parker@hotmail.com>
2024-06-02 21:17:24 -0230

    began work on assignment 02 part 03

---
+++ src/main/java/Assignments/A2Serialization/EricsFitnessProgram/FitnessProgram.java
@@ -0,0 +1,15 @@
+package Assignments.A2Serialization.EricsFitnessProgram;
+
+import java.nio.file.*;
+import java.util.Scanner;
+
+public class FitnessProgram {
+    public static void main(String[] args) {
+        Path path = Paths.get("src/main/java/Assignments/A2Serialization/EricsFitnessProgram/Settings.xml");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Press 1 to View Settings...");
+        System.out.println("Press 2 to Update Settings...");
+    
+    
+    } 
+}
