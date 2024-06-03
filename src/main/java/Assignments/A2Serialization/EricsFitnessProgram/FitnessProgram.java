ebd36ebacb405b946efd3a290543ba4ff5e02eaf
Parker.Wallace <wallace.parker@hotmail.com>
2024-06-03 12:30:17 -0230

    completed Assignment 02

--- src/main/java/Assignments/A2Serialization/EricsFitnessProgram/FitnessProgram.java
+++ src/main/java/Assignments/A2Serialization/EricsFitnessProgram/FitnessProgram.java
@@ -1,6 +1,10 @@
 package Assignments.A2Serialization.EricsFitnessProgram;
 
+import javax.xml.bind.JAXB;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.OutputStream;
 import java.nio.file.*;
 import java.util.Scanner;
 
@@ -17,9 +21,42 @@ public class FitnessProgram {
             switch (userChoice) {
                 case 1:
                     if (Files.exists(path)) {
-                        BufferedReader reader = Files.newBufferedReader(path);
-                        
-                    }       
+                        try (BufferedReader reader = Files.newBufferedReader(path)) {
+                            Settings settings = JAXB.unmarshal(reader, Settings.class);
+                            System.out.println(settings.getName());
+                            System.out.println(settings.getHeight());
+                            System.out.println(settings.getWeight());
+                            System.out.println(settings.getBirthday());
+                            System.out.println(settings.getFTP());
+                        }
+                        catch (Exception e) {
+                            System.out.println(e);
+                        }
+                    }
+                    else {
+                        System.out.println("Settings have not been saved yet, please update and save settings");
+                    }
+                    break;
+                case 2:
+                    try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+                        Settings settings = new Settings();
+                        Scanner userInput = new Scanner(System.in);
+                        System.out.println("please enter name");
+                        settings.setName(userInput.next());
+                        System.out.println("please enter height");
+                        settings.setHeight(userInput.nextInt());
+                        System.out.println("please enter weight");
+                        settings.setWeight(userInput.nextInt());
+                        System.out.println("please enter birthday");
+                        settings.setBirthday(userInput.next());
+                        System.out.println("please enter ftp");
+                        settings.setFtp(userInput.nextInt());
+                        JAXB.marshal(settings, writer);
+                    }
+                    catch (Exception e) {
+                        System.out.println(e);
+                    }
+                case 3: continue;
                 default:
                     System.out.println("invalid input please try again...");
             }
--- src/main/java/Assignments/A2Serialization/EricsFitnessProgram/Settings.java
+++ src/main/java/Assignments/A2Serialization/EricsFitnessProgram/Settings.java
@@ -2,8 +2,11 @@ package Assignments.A2Serialization.EricsFitnessProgram;
 
 import java.util.Date;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 
+@XmlAccessorType (XmlAccessType.FIELD)
 public class Settings {
     @XmlElement(name = "Name")
     private String name;
@@ -15,28 +18,48 @@ public class Settings {
     private int height;
 
     @XmlElement(name = "Birthday")
-    private Date birthday;
+    private String birthday;
 
     @XmlElement(name = "FTP")
     private int ftp;
 
     public String getName() {
-        return name;
+        return this.name;
     }
     
     public int getHeight() {
-        return height;
+        return this.height;
     }
     
     public int getWeight() {
-        return weight;
+        return this.weight;
     }
     
-    public Date getBirthday() {
-        return birthday;
+    public String getBirthday() {
+        return this.birthday;
     }
     
     public int getFTP() {
-        return ftp;
+        return this.ftp;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setFtp(int ftp) {
+        this.ftp = ftp;
     }
 }
