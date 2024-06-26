68c3a04b1b232fb224fc0dca420b30e8f8d7e919
Parker.Wallace <wallace.parker@hotmail.com>
2024-06-26 12:12:01 -0230

    finished A4

--- src/main/java/Assignments/A4/PriorityQueueTest.java
+++ src/main/java/Assignments/A4/PriorityQueueTest.java
@@ -1,13 +1,15 @@
 package Assignments.A4;
 
+import java.util.Comparator;
 // Fig. 16.14: PriorityQueueTest.java
 // PriorityQueue test program.
 import java.util.PriorityQueue;
 
 public class PriorityQueueTest {
    public static void main(String[] args) {
-      // queue of capacity 11                             
-      PriorityQueue<Double> queue = new PriorityQueue<>();
+      // queue of capacity 11
+      // PriorityQueue<Double> queue = new PriorityQueue<>();                             
+      PriorityQueue<Double> queue = new PriorityQueue<>(Comparator.reverseOrder());
 
       // insert elements to queue
       queue.offer(3.2);          
@@ -15,7 +17,7 @@ public class PriorityQueueTest {
       queue.offer(5.4);          
 
       System.out.print("Polling from queue: ");
-
+      
       // display elements in queue
       while (queue.size() > 0) {
          System.out.printf("%.1f ", queue.peek()); // view top element
--- src/main/java/Assignments/A4/ReverseLinkedList.java
+++ src/main/java/Assignments/A4/ReverseLinkedList.java
@@ -1,15 +1,32 @@
 package Assignments.A4;
 
-// Fig. 16.14: PriorityQueueTest.java
-// PriorityQueue test program.
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Iterator;
 
+/**
+ * creates a linkedlist of 10 characters a-j, and a second linkedlist containing the contents of the first reversed
+ */
 public class ReverseLinkedList {
     public static void main(String[] args) {
-        List<Character> tenCharacters = new LinkedList<Character>(Arrays.asList('a','b','c','d','e','f','g','h','i','j'));
-        LinkedList<Character> tenCharactersCopy = tenCharacters.clone();
+        //create both linkedlists
+        LinkedList<Character> tenCharacters = new LinkedList<Character>(Arrays.asList('a','b','c','d','e','f','g','h','i','j'));
+        LinkedList<Character> tenCharactersCopy = new LinkedList<>();
+
+        //create an iterator to go over each element in tencharacters starting with the last element
+        Iterator<Character> iterator = tenCharacters.iterator();
+        int index = tenCharacters.size() - 1;
+        while (iterator.hasNext()) {
+            Character character = tenCharacters.get(index);
+            tenCharactersCopy.add(character);
+            if (index > 0) {
+                index --;
+            }
+            else {
+                break;
+            }
+        }
+        System.out.println(tenCharacters);
+        System.out.println(tenCharactersCopy);
     }
     }
