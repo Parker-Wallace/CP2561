ba6c40992dd1b3b44b40f1aa929f85657c415050
Parker.Wallace <wallace.parker@hotmail.com>
2024-07-31 12:26:45 -0230

    added GUI.java to handle graphical aspects of CLI

---
+++ src/main/java/Projects/CardGameCLI/Client/GUI.java
@@ -0,0 +1,12 @@
+package Projects.CardGameCLI.Client;
+
+public class GUI {
+    final public static void GAMEHEADER() {}
+    final public static void GAMEWelcome() {
+        System.out.println("****************************************");
+        System.out.println("");
+        System.out.println("          WELCOME TO BLACKJACK              ");
+        System.out.println("");
+        System.out.println("*****************************************");
+    }
+}
